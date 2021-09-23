a7e16f90c201992ffb69ef07a158896ed3199ad1
matheussanchez <matheus.sanchez@ifood.com.br>
2021-09-23 14:45:23 -0300

    Games Repository working

--- chapter03-01-DataBaseQueries/src/modules/games/repositories/implementations/GamesRepository.ts
+++ chapter03-01-DataBaseQueries/src/modules/games/repositories/implementations/GamesRepository.ts
@@ -13,18 +13,27 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
-    return this.repository
-      .createQueryBuilder()
+    const gamesWithParam = await this.repository
+    .createQueryBuilder("games")
+    .where("games.title ILIKE :param", {param: `%${param}%`})
+    .getMany();
+
+    return gamesWithParam;
       // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    const lenghtGames = await this.repository.query("SELECT COUNT(*) FROM games");
+    return lenghtGames; // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
+
+    const gameFound = await this.repository
+    .createQueryBuilder("game").where("game.id = :id", { id })
+    .relation(Game, "users").of(id).loadMany();
+
+    return gameFound;
       // Complete usando query builder
   }
 }
